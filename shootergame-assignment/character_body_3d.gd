9f6519002ed8c5d0c498448ce271ba2124d4651c
lastsonofviltrum <charlieryan1515@gmail.com>
2025-05-07 22:22:57 +0100

    may 7 10"22 pm

--- shootergame-assignment/character_body_3d.gd
+++ shootergame-assignment/character_body_3d.gd
@@ -29,3 +29,39 @@ func _physics_process(delta: float) -> void:
 # i ask for nothing but a working camera is too much to ask for
 
 	move_and_slide()
+
+@export var score = 0
+@export var speed:float = 10
+var controlling = true
+
+var relative:Vector2 = Vector2.ZERO
+
+func _input(event):
+	if event is InputEventMouseMotion and controlling:
+		relative = event.relative
+	if event.is_action_pressed("ui_cancel"):
+		if controlling:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:            
+			Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		controlling = ! controlling
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	pass # Replace with function body.
+
+@export var can_move:bool = true
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	rotate(Vector3.DOWN, deg_to_rad(relative.x * deg_to_rad(rot_speed) * delta))
+	rotate(transform.basis.x,deg_to_rad(- relative.y * deg_to_rad(rot_speed) * delta))
+	relative = Vector2.ZERO
+	if can_move:
+		var v = Vector3.ZERO
+		
+		var mult = 1
+		if Input.is_key_pressed(KEY_SHIFT):
+			mult = 3
+		
--- shootergame-assignment/character_body_3d_2.gd
+++ shootergame-assignment/character_body_3d_2.gd
@@ -1,41 +1,6 @@
 extends Camera3D
 
-@export var score = 0
-@export var speed:float = 10
-@export var rot_speed = 500
-var controlling = true
-
-var relative:Vector2 = Vector2.ZERO
-
-func _input(event):
-	if event is InputEventMouseMotion and controlling:
-		relative = event.relative
-	if event.is_action_pressed("ui_cancel"):
-		if controlling:
-			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		else:            
-			Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-		controlling = ! controlling
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	pass # Replace with function body.
-
-@export var can_move:bool = true
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	rotate(Vector3.DOWN, deg_to_rad(relative.x * deg_to_rad(rot_speed) * delta))
-	rotate(transform.basis.x,deg_to_rad(- relative.y * deg_to_rad(rot_speed) * delta))
-	relative = Vector2.ZERO
-	if can_move:
-		var v = Vector3.ZERO
-		
-		var mult = 1
-		if Input.is_key_pressed(KEY_SHIFT):
-			mult = 3
-		
 	if Input.is_action_just_pressed("mouse_down"):
 		$AudioStreamPlayer3D.play()
 	if Input.is_action_just_released("mouse_down"):
